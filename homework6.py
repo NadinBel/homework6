64e637289d2d96ce69d78b3ae61c9bb7c6c6f014
NadinBel <143278036+NadinBel@users.noreply.github.com>
2024-05-15 23:35:19 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,11 @@
+my_list = ['grape', 'apple', 'watermelon', 'peach', 'orange', 'plum']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[1] = 'pear'
+print(my_list)
+my_diсt = {'grape':'виноград', 'apple':'яблоко', 'watermelon':'арбуз', 'peach':'персик'}
+print(my_diсt)
+print(my_diсt.get('peach'))
+my_diсt.update({'banana':'банан'})
+print(my_diсt)
